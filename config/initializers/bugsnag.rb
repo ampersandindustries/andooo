c10ccf429c0c1e4f0d30ceee7dc18464bac7e016
e.a. nakashima <e.a.nakashima@gmail.com>
2016-04-24 13:56:53 -0700

    Set up Bugsnag for exception monitoring

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
 Bugsnag.configure do |config|
-  config.api_key = ENV['BUGSNAG_KEY']
+  config.api_key = "25494b0dde8f7d4d7594349f1bfdece9"
   config.notify_release_stages = ["staging", "production"]
 end
