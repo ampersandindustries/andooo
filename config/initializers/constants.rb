3a3306d7d4220a0ef15f330becb5212cb1ffb4f7
e.a. nakashima <e.a.nakashima@gmail.com>
2016-04-24 13:48:02 -0700

    Use AndConf's Google Analytics id

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,4 +4,4 @@ POLICIES_URL         = SITE_URL + "/code_of_conduct"
 MAILING_LIST         = "http://andconf.us11.list-manage1.com/subscribe?u=3326a341f7c38e3b32a324595&id=d4f6e283fb"
 CARPOOL_DOC          = "https://docs.google.com/spreadsheets/d/1mS6jM6CtcQnFeNbbbJILr7ssPVYwxzGXgrha7iyTIiI/edit?usp=sharing"
 
-GOOGLE_ANALYTICS_ID  = 'UA-73432209-1'
+GOOGLE_ANALYTICS_ID  = 'UA-62820008-1'
