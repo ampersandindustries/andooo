3ec2bd5baaefad13d19729dade92a4fb4f16fb5d
e.a. nakashima <e.a.nakashima@gmail.com>
2016-05-02 22:12:12 -0700

    No members here

--- spec/features/voting_spec.rb
+++ spec/features/voting_spec.rb
@@ -10,28 +10,28 @@ describe "voting on an application" do
   context "when logged in as a application reviewer" do
     let(:application_reviewer) { create(:application_reviewer) }
 
-    it "allows the member to vote yes" do
+    it "allows the voter to vote yes" do
       visit volunteers_application_path(application)
-      expect(page).to have_content "Votes for membership (0)"
+      expect(page).to have_content "Votes for attendance (0)"
       click_button "Yes"
-      expect(page).to have_content "Votes for membership (1)"
+      expect(page).to have_content "Votes for attendance (1)"
       expect(page).to have_content "You voted yes"
     end
 
-    it "allows the member to vote no" do
+    it "allows the voter to vote no" do
       visit volunteers_application_path(application)
-      expect(page).to have_content "Votes against membership (0)"
+      expect(page).to have_content "Votes against attendance (0)"
       click_button "No"
-      expect(page).to have_content "Votes against membership (1)"
+      expect(page).to have_content "Votes against attendance (1)"
       expect(page).to have_content "You voted no"
     end
 
-    it "allows the member to remove their vote" do
+    it "allows the voter to remove their vote" do
       visit volunteers_application_path(application)
       click_button "No"
-      expect(page).to have_content "Votes against membership (1)"
+      expect(page).to have_content "Votes against attendance (1)"
       click_link "Remove your vote"
-      expect(page).to have_content "Votes against membership (0)"
+      expect(page).to have_content "Votes against attendance (0)"
     end
   end
 end
