5b7d577552efccb1ca36c6b42a19c5eddfa90d79
e.a. nakashima <e.a.nakashima@gmail.com>
2016-05-29 10:29:45 -0700

    I am bad, I feel bad

--- spec/features/confirming_attendance_spec.rb
+++ spec/features/confirming_attendance_spec.rb
@@ -63,7 +63,7 @@ def fill_in_the_deets
   fill_in "Gender", with: "NB"
   fill_in "Pronouns", with: "they/their"
   check "attendance_dietary_restrictions_vegan"
-  fill_in "Additional dietary restrictions or concerns (e.g. needing access to a fridge)", with: "Need access to a kitchen"
+  fill_in "Additional dietary restrictions or concerns (e.g. needing access to a fridge to store medication)", with: "Need access to a kitchen"
   fill_in "Twitter Handle", with: "@fun_times"
   select "I have no preference", from: "Sleeping Preferences"
   select "Maybe", from: "Are you staying at St. Dorothy's Rest on Sunday night?"
