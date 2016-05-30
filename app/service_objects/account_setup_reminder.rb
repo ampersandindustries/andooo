dd4789e9f37ad4b995449c1a065c706270f01caf
e.a. nakashima <e.a.nakashima@gmail.com>
2016-05-29 19:28:50 -0700

    Log sending reminder emails in Bugsnag

--- app/service_objects/account_setup_reminder.rb
+++ app/service_objects/account_setup_reminder.rb
@@ -9,11 +9,30 @@ class AccountSetupReminder
       processed_at = user.application.processed_at
       if processed_at < 2.days.ago && processed_at > 4.days.ago
         SignupReminderMailer.three_day_reminder(user).deliver_now
+        log_to_bugsnag(3, user.email)
       elsif processed_at < 5.days.ago && processed_at > 7.days.ago
         SignupReminderMailer.six_day_reminder(user).deliver_now
+        log_to_bugsnag(6, user.email)
       elsif processed_at < 8.days.ago && processed_at > 10.days.ago
         SignupReminderMailer.nine_day_reminder(user).deliver_now
+        log_to_bugsnag(9, user.email)
       end
+    end
+  end
+
+  private
+
+  def log_to_bugsnag(day_num, email)
+    err = BugsnagError.new("Sent #{day_num}-day reminder email")
+
+    err.bugsnag_meta_data = {
+      user_info: {
+        email: email
+      }
+    }
+
+    Bugsnag.notify(err) do |notif|
+      notif.severity = 'info'
     end
   end
 end
--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,3 +2,7 @@ Bugsnag.configure do |config|
   config.api_key = "25494b0dde8f7d4d7594349f1bfdece9"
   config.notify_release_stages = ["staging", "production"]
 end
+
+class BugsnagError < RuntimeError
+  include Bugsnag::MetaData
+end
