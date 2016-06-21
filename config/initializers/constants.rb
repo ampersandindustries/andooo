6bbc5e1dfefaab04e109cdc5af52781cb898d3a2
Lillie Chilen <lillie.chilen@gmail.com>
2016-06-20 17:45:58 -0700

    Add transit email address to attendee page
    - Move Slack channel link into a constant

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,10 +1,12 @@
 INFO_EMAIL           = "info@andconf.io"
 ATTEND_EMAIL         = "attend@andconf.io"
+TRANSPORTATION_EMAIL = "transportation@andconf.io"
 SITE_URL             = "https://www.andconf.io"
 POLICIES_URL         = SITE_URL + "/code_of_conduct"
 MAILING_LIST         = "http://andconf.us11.list-manage1.com/subscribe?u=3326a341f7c38e3b32a324595&id=d4f6e283fb"
 CARPOOL_DOC          = "https://docs.google.com/spreadsheets/d/1mS6jM6CtcQnFeNbbbJILr7ssPVYwxzGXgrha7iyTIiI/edit?usp=sharing"
 SLACK_CHANNEL        = "http://andconf.slack.com"
+TRANSIT_SLACK_CHANNEL= SLACK_CHANNEL + "/messages/transportation/"
 GOOGLE_ANALYTICS_ID  = 'UA-62820008-1'
 TWITTER_URL          = "https://twitter.com/andconf"
 BLOG_URL             = "https://blog.andconf.io"
