df87f16f662cea6e440cf717d4ae368ddb42c895
Stella Cotton <stella@stellacotton.com>
2016-07-31 09:55:24 -0700

    Add attendance transportation reminder email and previews

--- app/mailers/attendances_mailer.rb
+++ app/mailers/attendances_mailer.rb
@@ -11,4 +11,13 @@ class AttendancesMailer < ActionMailer::Base
       subject: "Finalize your AndConf details by #{Configurable[:attendance_updates_close_date]}"
     )
   end
+
+  def transportation_confirmation(attendance)
+    @attendance = attendance
+
+    mail(
+      to: attendance.user.email,
+      subject: "Transportation Details for AndConf"
+    )
+  end
 end
